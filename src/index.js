cee2294c5c6cec83c4a4e00aee78b3c3f30aa345
Gustavo Gonzalez <gustavog525@gmail.com>
2019-02-02 13:39:13 -0500

    works?

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {GoogleAPI,GoogleLogin,GoogleLogout} from 'react-google-oauth'
 
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+ReactDOM.render(
+        <GoogleAPI clientId="1033991025049-hq3385qts2q4be91i9asu8fhs15jkpur.apps.googleusercontent.com"
+        scope="https://www.googleapis.com/auth/youtube.readonly"
+    >
+			<div>
+              	<div><GoogleLogin /></div>
+              	<div><GoogleLogout /></div>
+    		</div>
+        </GoogleAPI>, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
